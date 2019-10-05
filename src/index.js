90f5b5a1a4da79d9cd9cfb271baaf35031bf7e06
xw <23066125+xwang1000@users.noreply.github.com>
2019-10-05 16:32:06 -0700

    Add style config and scheme files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './scheme/config.scss'
 import './scheme/scheme.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
