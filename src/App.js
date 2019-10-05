ba70f312e268d392a3222da44d6c683f71210b90
aryyya <aryyya.xyz@gmail.com>
2019-10-05 16:29:26 -0700

    Add ESLint with AirBnB configuration and manual overrides.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,85 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import './App.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.scss';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/dance/bellydance">Bellydance</Link>
-            </li>
-            <li>
-              <Link to="/dance/bollywooddance">Bollywood</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+const Dance = ({ match }) => (
+  <div>
+    <h2>Dance</h2>
+    <p>{match.params.danceId}</p>
+  </div>
+);
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dance/:danceId" component={Dance}>
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  )
-}
+Dance.propTypes = {
+  match: PropTypes.object.isRequired,
+};
 
-const Home = () => {
-  return <h2>Home</h2>
-}
+const User = ({ name, age }) => (
+  <div>
+    <div>{name}</div>
+    {
+      age && (
+        <div>
+          {age} years old
+        </div>
+      )
+    }
+  </div>
+);
 
-const Dance = ({ match }) => {
-  return (
-    <div>
-      <h2>Dance</h2>
-      <p>{match.params.danceId}</p>
-    </div>
-  )
-}
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number
+};
 
-const About = () => {
-  return <h2>About Me</h2>
-}
+User.defaultProps = {
+  age: null
+};
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <User name="Arya" age={27} />
+    <User name="Xuenan" />
+  </div>
+);
+
+const About = () => (
+  <h2>About Me</h2>
+);
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dance/bellydance">Bellydance</Link>
+          </li>
+          <li>
+            <Link to="/dance/bollywooddance">Bollywood</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/dance/:danceId" component={Dance} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
