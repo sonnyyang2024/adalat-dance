a8678179686ca643d06a0de605f8233403f8478b
aryyya <aryyya.xyz@gmail.com>
2019-10-06 12:03:34 -0700

    Disable prefer default export rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,13 +22,16 @@ module.exports = {
     'react',
   ],
   rules: {
-    // JavaScript Rules
+    // js rules
     'comma-dangle': 0,
 
-    // React Rules
+    // import rules
+    'import/prefer-default-export': 0,
+
+    // react rules
     'react/forbid-prop-types': 0,
 
-    // JSX Rules
+    // jsx rules
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-filename-extension': [
       1,
