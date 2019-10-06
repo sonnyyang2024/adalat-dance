2045342d85b629524d923956a568f8cd3c89b3d7
xw <23066125+xwang1000@users.noreply.github.com>
2019-10-06 16:26:26 -0700

    Address pull request comments

--- src/components/pages/dance-page/CourseCard.js
+++ src/components/pages/dance-page/CourseCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// hooks
 import useLanguage from '../../common/useLanguage';
 
+// styles
 import './CourseCard.scss';
 
 const CourseCard = ({
