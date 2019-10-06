106fa373e8742f1e6baf7819dd1a99d778ba533c
aryyya <aryyya.xyz@gmail.com>
2019-10-06 15:53:05 -0700

    Remove unused hook and use new one.

---
+++ src/components/pages/home-page/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { getImageUrl } from '../../../utility';
+
+// hooks
+import useLanguage from '../../common/useLanguage';
+
+// data
+import translations from './translations';
+
+// components
+import Button from '../../common/Button';
+
+const HomePage = () => {
+  const language = useLanguage();
+  const { title, description, button } = translations[language];
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <Button text={button} />
+      <img
+        src={getImageUrl('ada-white.jpg')}
+        alt="Ada in white bellydance costum"
+        width="500"
+        height="600"
+      />
+    </div>
+  );
+};
+
+export default HomePage;
