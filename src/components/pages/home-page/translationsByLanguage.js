d314ef1c657b5ded36d4e25d4405ec14405e1956
xw <23066125+xwang1000@users.noreply.github.com>
2019-10-06 22:13:44 -0700

    Update translationsByLanguage.js

--- src/components/pages/home-page/translationsByLanguage.js
+++ src/components/pages/home-page/translationsByLanguage.js
@@ -1,12 +1,24 @@
+import React, { Fragment } from 'react';
+
 export default {
   en: {
-    title: 'Hello, world!',
-    description: 'This is the home page.',
-    button: 'Learn More'
+    title: 'Adalat Omar',
+    description: (
+      <Fragment>
+        <p>A Bellydancer, Uyghur and Bollywood dance lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Nulla vestibulum velit nunc, sed ultrices ex faucibus sit amet.</p>
+      </Fragment>
+    ),
+    button: 'Contact Adalat'
   },
   ch: {
-    title: '你好!',
-    description: '这是主页.',
-    button: '了解更多'
+    title: 'Adalat Omar',
+    description: (
+      <Fragment>
+        <p>Adalat是一个专业肚皮舞、维吾尔舞、宝莱坞舞蹈艺术家。天地玄黄宇宙洪荒，日月辰宿列张。寒来暑秋收冬藏。成岁，律吕调阳。</p>
+        <p>云腾致雨露结，为霜金生丽玉出昆冈。</p>
+      </Fragment>
+    ),
+    button: '联系Adalat'
   }
 };
