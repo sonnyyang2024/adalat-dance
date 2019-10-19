cf533a251a7dd1aa13a4c58e58514c1f1d937f9a
xw <23066125+xwang1000@users.noreply.github.com>
2019-10-19 11:52:25 -0700

    Add placeholder content

--- src/components/pages/dance-page/DancePage.js
+++ src/components/pages/dance-page/DancePage.js
@@ -11,23 +11,26 @@ import dataByLanguage from './dataByLanguage';
 import Button from '../../common/button/Button';
 import CourseList from './course-list/CourseList';
 
+import './DancePage.scss';
+
 const DancePage = ({
   match
 }) => {
-  const data = useDataByLanguage(dataByLanguage);
+  // const data = useDataByLanguage(dataByLanguage);
 
-  const { danceId } = match.params;
-  const dance = data[danceId];
+  // const { danceId } = match.params;
+  // const dance = data[danceId];
+  // <h1>{dance.title}</h1>
+  // <p>{dance.description}</p>
+  // <Button
+  //   text={dance.button}
+  //   link="/"
+  // />
+  // <CourseList courses={dance.courses} />
 
   return (
-    <div>
-      <h1>{dance.title}</h1>
-      <p>{dance.description}</p>
-      <Button
-        text={dance.button}
-        link="/"
-      />
-      <CourseList courses={dance.courses} />
+    <div className="dance-page">
+      <h1>See mockup</h1>
     </div>
   );
 };
